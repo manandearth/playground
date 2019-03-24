a6d16e4610d8332ebcc08d3ed82ef718497c0ae4
manandearth <adamgefen@posteo.net>
2019-03-24 09:11:59 +0100

    directs `delete` and `retrieve` in `/invoices` table using `url-for`

--- src/playground/services/invoices/retrieve_all/view.clj
+++ src/playground/services/invoices/retrieve_all/view.clj
@@ -2,6 +2,7 @@
   (:require
    [hiccup.page :as page]
    [hiccup.table :as table]
+   [io.pedestal.http.route :refer [url-for]]
    [playground.views :as views]))
 
 (defn all-invoices [context]
@@ -14,12 +15,15 @@
      (let [attr-fns {:data-value-transform
                      (fn [label-key v]
                        (if (= :id label-key)
-                         [:a {:href (str "/invoices/" v)} v]
+                         [:a {:href (url-for :invoices/:id :path-params {:id v})} v]
                          (if (= :delete label-key)
-                           [:a {:href (str "/invoices-delete/" v)} "delete"]
+                           [:a {:href
+                                  (url-for :invoices-delete/:id :path-params {:id v})}"delete"]
                            v)))}
            extended-context (map #(assoc % :delete (:id %)) context)]
        (table/to-table1d
         extended-context
         [:id "ID" :email "Email" :delete "to-delete"]
         attr-fns))]]))
+
+
