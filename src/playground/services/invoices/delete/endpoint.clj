b33507e3334b0dfb6739770e99f124ca5965d0cc
manandearth <adamgefen@posteo.net>
2019-05-05 08:19:06 +0200

    Improve `delete.endpoint` style

--- src/playground/services/invoices/delete/endpoint.clj
+++ src/playground/services/invoices/delete/endpoint.clj
@@ -11,9 +11,9 @@
 (spec/def ::api (spec/keys :req-un [::models.user/id]))
 
 (defn perform [{{:keys [id]} :path-params :keys [db session] :as request}]
-  (let [db (->> db :pool (hash-map :datasource))
-        _  (->> (logic/to-delete id)
-                (h/format)
-                (jdbc/execute! db))]
+  (let [db (->> db :pool (hash-map :datasource))]
+    (->> (logic/to-delete id)
+         (h/format)
+         (jdbc/execute! db))
     (-> (ring-resp/redirect (url-for :invoices))
         (assoc :flash (str "Entry " id " has beed deleted.")))))
